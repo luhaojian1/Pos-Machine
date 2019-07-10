eecbb30a0d5e036c717f7f4b2f41c6030ea3970f
LUAL6 <962671114@qq.com>
2019-07-10 12:02:38 +0800

    review code

--- __tests__/Receipt-spec.js
+++ __tests__/Receipt-spec.js
@@ -1,68 +1,66 @@
 const receipt = require("../receipt/Receipt");
-
-it('should  return a array when invoke isValid given barcodes=[\'0001\', \'0003\', \'0005\', \'0003\']', function () {
+//countGoods测试
+it('should  return a array when invoke countGoods given barcodes=[\'0001\', \'0003\', \'0005\', \'0003\']', function () {
     //given
-   const barcodes = ['0001', '0003', '0005', '0003'];
+    const barcodes = ['0001', '0003', '0005', '0003'];
     //when
-    const result = receipt.arrangeGoods(barcodes);
+    const result = receipt.countGoods(barcodes);
     //then
-    expect(result).toEqual([{id:'0001',count:1},{id:'0003',count:2},{id:'0005',count:1}]);
-
+    expect(result).toEqual([{id: '0001', count: 1}, {id: '0003', count: 2}, {id: '0005', count: 1}]);
 });
-//isExist测试-》 false
-it('should be return false array when invoke isExist given barcodeChildElement=\'0011\'', function () {
+//isExist测试
+it('should be return false when invoke isExist given barcodeChildElement=\'0011\'', function () {
     //given
-    const barcodeChildElement='0011';
+    const barcodeChildElement = '0011';
     //when
     const result = receipt.isExists(barcodeChildElement);
     //then
     expect(result).toBe(false);
-
 });
-it('should be return  true when invoke isExist given barcodeChildElement=\'0003\'', function () {
+it('should be return true when invoke isExist given barcodeChildElement=\'0003\'', function () {
     //given
-    const barcodeChildElement='0003';
+    const barcodeChildElement = '0003';
     //when
     const result = receipt.isExists(barcodeChildElement);
     //then
     expect(result).toBe(true);
-
 });
 
-//isExist测试-》 false
+//getDataSource测试
 it('should  return null when invoke getDataSource given barcodeChildElement=\'0011\'', function () {
     //given
-    const barcodeChildElement='0011';
+    const barcodeChildElement = '0011';
     //when
     const result = receipt.getDataSource(barcodeChildElement);
     //then
     expect(result).toBe(null);
-
 });
+
+//getDataSource测试
 it('should  return a Good Object when invoke getDataSource given barcodeChildElement=\'0011\'', function () {
     //given
-    const barcodeChildElement='0001';
+    const barcodeChildElement = '0001';
     //when
     const result = receipt.getDataSource(barcodeChildElement);
     //then
-    expect(result).toEqual({"id": "0001", "name" : "Coca Cola", "price": 3});
-
+    expect(result).toEqual({"id": "0001", "name": "Coca Cola", "price": 3});
 });
-//createReceipt测试 错误：
+
+//createReceipt测试
 it('should  return \'错误\' when invoke createReceipt given barcodes=[{id:\'0001\',count:1},{id:\'0002\',count:2}]', function () {
     //given
-    const barcodes =[{id:'0001',count:1},{id:'0012',count:2}];
+    const barcodes = [{id: '0001', count: 1}, {id: '0012', count: 2}];
     //when
     const result = receipt.createReceipt(barcodes);
     console.log(result);
     //then
     expect(result).toBe("错误！barcodes：0012 不存在");
-
 });
-//createReceipt测试 正确：
+
+//createReceipt测试
 it('should  return \'错误\' when invoke createReceipt given barcodes=[{id:\'0001\',count:1},{id:\'0003\',count:2}，{id:\'0005\',count:1}]', function () {
     //given
-    const barcodes =[{id:'0001',count:1},{id:'0003',count:2},{id:'0005',count:1}];
+    const barcodes = [{id: '0001', count: 1}, {id: '0003', count: 2}, {id: '0005', count: 1}];
     //when
     const result = receipt.createReceipt(barcodes);
     //then
@@ -74,12 +72,12 @@ it('should  return \'错误\' when invoke createReceipt given barcodes=[{id:\'00
         "Dr Pepper\t7\t1\n" +
         "------------------------------------------------------------\n" +
         "Price：20");
-
 });
-//createReceipt测试 正确：
+
+//printReceipt测试
 it('should  return a receipt when invoke printReceipt given barcodes=[\'0001\', \'0003\', \'0005\', \'0003\']', function () {
     //given
-    const barcodes =['0001', '0003', '0005', '0003'];
+    const barcodes = ['0001', '0003', '0005', '0003'];
     //when
     const result = receipt.printReceipt(barcodes);
     //then
@@ -91,5 +89,4 @@ it('should  return a receipt when invoke printReceipt given barcodes=[\'0001\', 
         "Dr Pepper\t7\t1\n" +
         "------------------------------------------------------------\n" +
         "Price：20");
-
 });
--- receipt/Receipt.js
+++ receipt/Receipt.js
@@ -1,80 +1,78 @@
-var goods =[
-    {"id": "0001", "name" : "Coca Cola", "price": 3},
-    {"id": "0002", "name" : "Diet Coke", "price": 4},
-    {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
-    {"id": "0004", "name" : "Mountain Dew", "price": 6},
-    {"id": "0005", "name" : "Dr Pepper", "price": 7},
-    {"id": "0006", "name" : "Sprite", "price": 8},
-    {"id": "0007", "name" : "Diet Pepsi", "price": 9},
-    {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
-    {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
-    {"id": "0010", "name" : "Fanta", "price": 12}
+var goods = [
+    {"id": "0001", "name": "Coca Cola", "price": 3},
+    {"id": "0002", "name": "Diet Coke", "price": 4},
+    {"id": "0003", "name": "Pepsi-Cola", "price": 5},
+    {"id": "0004", "name": "Mountain Dew", "price": 6},
+    {"id": "0005", "name": "Dr Pepper", "price": 7},
+    {"id": "0006", "name": "Sprite", "price": 8},
+    {"id": "0007", "name": "Diet Pepsi", "price": 9},
+    {"id": "0008", "name": "Diet Mountain Dew", "price": 10},
+    {"id": "0009", "name": "Diet Dr Pepper", "price": 11},
+    {"id": "0010", "name": "Fanta", "price": 12}
 ];
 
 //打印账单
 function printReceipt(barcodes) {
-   let result = arrangeGoods(barcodes);
-   let report = createReceipt(result);
-   return report;
+    const barcodesObjectList = countGoods(barcodes);
+    const report = createReceipt(barcodesObjectList);
+    return report;
 }
 
 //判断商品Id是否存在
 function isExists(barcodeChildElement) {
-    let result = getDataSource(barcodeChildElement);
+    const result = getDataSource(barcodeChildElement);
     if (result == null)
         return false;
     else return true;
 }
+
 //创建账单
 function createReceipt(barcodes) {
     console.log(barcodes);
     let sumMoney = 0;
-   let result = "Receipts\n";
-   result +="------------------------------------------------------------\n";
-   for (let i=0; i<barcodes.length; i++){
-       if (!isExists(barcodes[i].id))
-           return "错误！barcodes："+barcodes[i].id+" 不存在";
-       let good = getDataSource(barcodes[i].id);
-       sumMoney += good.price*barcodes[i].count;
-       result += good.name+"\t"+good.price+"\t"+barcodes[i].count+"\n";
-   }
-   result += "------------------------------------------------------------\n";
-    result += "Price："+sumMoney;
-   return result;
+    let result = "Receipts\n";
+    result += "------------------------------------------------------------\n";
+    for (let i = 0; i < barcodes.length; i++) {
+        if (!isExists(barcodes[i].id))
+            return "错误！barcodes：" + barcodes[i].id + " 不存在";
+        const good = getDataSource(barcodes[i].id);
+        sumMoney += good.price * barcodes[i].count;
+        result += good.name + "\t" + good.price + "\t" + barcodes[i].count + "\n";
+    }
+    result += "------------------------------------------------------------\n";
+    result += "Price：" + sumMoney;
+    return result;
 }
 
 
 //获取货物信息
 function getDataSource(barcodeChildElement) {
-    for (let i=0;i<goods.length;i++){
+    for (let i = 0; i < goods.length; i++) {
         if (barcodeChildElement == goods[i].id)
             return goods[i];
     }
     return null;
 }
 
-//整理商品
-function arrangeGoods(barcodes) {
-    let i=0;
+//整理，计算商品数量，返回一个包含商品id和数量的对象的集合
+function countGoods(barcodes) {
+    let i = 0;
     let goodObject = {};
     let arr = [];
-    for (i = 0; i<barcodes.length;i++){
-        if (goodObject[barcodes[i]]==undefined)
-        {
+    for (i = 0; i < barcodes.length; i++) {
+        if (goodObject[barcodes[i]] == undefined) {
             goodObject[barcodes[i]] = 1;
             arr.push(barcodes[i]);
-        }
-        else goodObject[barcodes[i]]++;
+        } else goodObject[barcodes[i]]++;
     }
     let result = [];
-    for (i = 0; i < arr.length; i++){
+    for (i = 0; i < arr.length; i++) {
         result.push({
-            id:arr[i],
-            count:goodObject[arr[i]]
-        }) ;
+            id: arr[i],
+            count: goodObject[arr[i]]
+        });
     }
-    console.log(result);
     return result;
 }
 
-module.exports = {printReceipt,createReceipt,arrangeGoods,isExists,getDataSource};
+module.exports = {printReceipt, createReceipt, countGoods, isExists, getDataSource};
